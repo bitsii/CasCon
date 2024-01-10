34b01983ba43e41e0fefc3587486316be6c0af6b
craig <craig@here>
2024-01-09 22:53:50 -0800

    add package for ha

---
+++ scripts/harel.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd "${0%/*}"
+
+cd ../../apprun/App
+
+cat CasCon/runParamsWa.txt | grep -v bindAddress | grep -v 127 > rpwt
+
+echo "--webApp.CasCon.app.port" >> rpwt
+echo "3200" >> rpwt
+
+cp rpwt CasCon/runParamsWa.txt
+cp rpwt CasCon/runParamsWaBr.txt
+rm rpwt
+
+rm -f CasCon.tar.gz
+
+tar -czvf CasCon.tar.gz CasCon
+
+

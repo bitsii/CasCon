0dcde8eb80327cd8bcf73cabd53490160cde6de2
craig <craig@here>
2024-07-16 17:40:37 -0700

    switch to CasInt

--- resources/bpinstall.sh
+++ resources/bpinstall.sh
@@ -5,8 +5,8 @@ export PATH=$PATH:/usr/bin:/usr/local/bin:/sbin:/usr/sbin:/usr/local/sbin:.
 export OSTYPE=`uname`
 
 echo "Getting required additional application software"
-cp App/BBridge/javax.servlet-api-*.jar App/CasCon
-cp App/BBridge/jetty-all-*-uber.jar App/CasCon
+cp App/CasInt/javax.servlet-api-*.jar App/CasCon
+cp App/CasInt/jetty-all-*-uber.jar App/CasCon
 
 cd App/CasCon
 unzip -o extlibs.zip
